1b873aac750a9ebc4491561e197713b9964ae8d2
chenyao <491005758@qq.com>
2022-03-20 19:54:52 +0800

    断路器-修改

--- parent/service-ribbon/src/main/java/com/cy/serviceribbon/HelloService.java
+++ parent/service-ribbon/src/main/java/com/cy/serviceribbon/HelloService.java
@@ -1,5 +1,6 @@
 package com.cy.serviceribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,9 +9,15 @@ import org.springframework.web.client.RestTemplate;
 public class HelloService {
     @Autowired
     RestTemplate restTemplate;
+    //断路器，远程调用失败的回调函数
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name)
     {
         return restTemplate.getForObject("http://service-support/"+"support?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 
 }
